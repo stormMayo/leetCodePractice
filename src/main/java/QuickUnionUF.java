0faec9f2841f10fe9062814dc789dabe394d5aac
stormMayo <stormmayol@gmail.com>
2019-11-08 00:00:33 +0800

    quickFinf and quickUnion

---
+++ src/main/java/QuickUnionUF.java
@@ -0,0 +1,26 @@
+public class QuickUnionUF {
+    private int[] id;
+    public QuickUnionUF(int N){
+        id = new int[N];
+        for(int i = 0 ; i < N ; i++){
+            id[i] = i;
+        }
+    }
+
+    private int root(int i){
+        while (id[i] != i){
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p,int q){
+        return root(p) == root(q);
+    }
+    public void union(int p,int q){
+        int rootp = root(p);
+        int rootq = root(q);
+        id[rootp] = rootq;
+    }
+
+}
