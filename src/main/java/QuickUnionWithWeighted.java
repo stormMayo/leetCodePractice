043ee9ca5f4408f11fe57d9a587f40f67ab2f705
stormMayo <stormmayol@gmail.com>
2019-11-11 22:38:36 +0800

    weighted quickUnion

---
+++ src/main/java/QuickUnionWithWeighted.java
@@ -0,0 +1,39 @@
+public class QuickUnionWithWeighted {
+    private int[] id;
+    private int[] sz;
+    public QuickUnionWithWeighted(int N){
+        id = new int[N];
+        sz = new int[N];
+        for(int i = 0 ; i < id.length ; i++){
+            id[i] = i;
+        }
+    }
+
+    private int root(int i){
+        while (id[i] != i){
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p,int q){
+        return root(p) ==root(q);
+    }
+
+    public void union(int p , int q){
+        int i = root(p);
+        int j = root(q);
+        if(i == j){
+            return;
+        }
+        if(sz[i] < sz[j]){
+            sz[j] += sz[i];
+            id[i] = j;
+        }else {
+            sz[i] += sz[j];
+            id[j] = i;
+        }
+
+    }
+
+}
