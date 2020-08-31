783775470da3992a4abcd6affbb53bbcf779ee9e
刘超 <stormmayol@gmail.com>
2020-08-31 14:21:45 +0800

    剑指10-fib

--- src/main/com.mayol/sword/_10_斐波那契数列.java
+++ src/main/com.mayol/sword/_10_斐波那契数列.java
@@ -1,20 +1,23 @@
-/**
- * @class _10_斐波那契数列
- * @package sword
- * @date 2020/8/31 13:45
- * Copyright (c) 2019 ZheJiang Loong Airlines CO.,Ltd, All Rights Reserved.
- * 本代码仅限于浙江长龙航空有限公司内部使用，禁止外泄，禁止用于其他任何商业目的。
- */
 package sword;
-
 /**
- *
- * @author chaoliu5008
- * @date 2020/8/31 13:45 
- * @version: V1.0
- * modifyTime           author              description
- * -------------------------------------------------------------------
+ *https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
  *
  */
+
 public class _10_斐波那契数列 {
+    public int fib(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        }
+        int a = 0;
+        int b = 1;
+        for(int i = 1 ; i < n ;i++){
+            b = a + b;
+            a = b - a;
+            if(b > 1000000007){
+                b -= 1000000007;
+            }
+        }
+        return b;
+    }
 }
