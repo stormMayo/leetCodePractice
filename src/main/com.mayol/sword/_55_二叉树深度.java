acc95f323d1d25067f8b45b0844e3058935fe3fb
刘超 <stormmayol@gmail.com>
2020-09-09 14:31:50 +0800

    剑指10-fib

--- src/main/com.mayol/sword/_55_二叉树深度.java
+++ src/main/com.mayol/sword/_55_二叉树深度.java
@@ -1,6 +1,36 @@
 
 package sword;
 
+import java.util.LinkedList;
+import java.util.Queue;
 
+/**
+ * https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+ */
 public class _55_二叉树深度 {
+    public int maxDepth(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null){
+            return 0;
+        }
+        queue.offer(root);
+        int depth = 0;
+        int count = 1;
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            count--;
+            if(node.left != null){
+                queue.offer(node.left);
+            }
+            if(node.right != null){
+                queue.offer(node.right);
+            }
+            if(count ==0){
+                count = queue.size();
+                depth++;
+            }
+        }
+        return depth;
+    }
+    
 }
